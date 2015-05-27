5835f20199a4f817a2c6c15019cd5ad56a95c82e
prateep.gedupudi <prateep.gedupudi@gmail.com>
2015-05-27 14:14:34 +0530

    Basic Authentication Filter

---
+++ src/main/java/com/prateep/app/library/filter/LibraryBasicAuthenticationFilter.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package com.prateep.app.library.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.prateep.app.library.service.LibraryAuthenticationService;
+
+/**
+ * @author prateep.gedupudi
+ *
+ */
+public class LibraryBasicAuthenticationFilter implements javax.servlet.Filter{
+	public static final String AUTHENTICATION_HEADER = "Authorization";
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain filter) throws IOException, ServletException {
+		if (request instanceof HttpServletRequest) {
+			HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+			String authCredentials = httpServletRequest
+					.getHeader(AUTHENTICATION_HEADER);
+
+			// better injected
+			LibraryAuthenticationService authenticationService = new LibraryAuthenticationService();
+
+			boolean authenticationStatus = authenticationService
+					.authenticate(authCredentials);
+
+			if (authenticationStatus) {
+				filter.doFilter(request, response);
+			} else {
+				if (response instanceof HttpServletResponse) {
+					HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+					httpServletResponse
+							.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+				}
+			}
+		}
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/com/prateep/app/library/service/LibraryAuthenticationService.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.prateep.app.library.service;
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.StringTokenizer;
+
+/**
+ * @author prateep.gedupudi
+ *
+ */
+public class LibraryAuthenticationService {
+
+	public boolean authenticate(String authCredentials) {
+		if (null == authCredentials)
+			return false;
+		// header value format will be "Basic encodedstring" for Basic
+		// authentication. Example "Basic YWRtaW46YWRtaW4="
+		final String encodedUserPassword = authCredentials.replaceFirst("Basic"
+				+ " ", "");
+		String usernameAndPassword = null;
+		try {
+			byte[] decodedBytes = Base64.getDecoder().decode(
+					encodedUserPassword);
+			usernameAndPassword = new String(decodedBytes, "UTF-8");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		final StringTokenizer tokenizer = new StringTokenizer(
+				usernameAndPassword, ":");
+		final String username = tokenizer.nextToken();
+		final String password = tokenizer.nextToken();
+
+		// we have fixed the userid and password as admin
+		// call some UserService/LDAP here
+		boolean authenticationStatus = "prateep".equals(username)
+				&& "prateep".equals(password);
+		return authenticationStatus;
+	}
+
+}
